90984ef01462a40f5f3328f3f18849573ed67a1b
Mousa Khaleel <mousakhaleel22@gmail.com>
2025-03-22 15:52:47 +0200

    initial file structure & design

---
+++ src/components/navigationBar/NavigationBar.js
@@ -0,0 +1,11 @@
+// import Logo from '../../assets/stockPhotos'
+// import Logo from '../../assets/stockPhoto/'
+
+function NavigationBar() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default NavigationBar;
