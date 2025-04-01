5c58da9342a9ea7567a0a3d0f39a9245afd56efe
Mousa Khaleel <mousakhaleel22@gmail.com>
2025-04-01 19:32:48 +0300

    refactoring and files initialization

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import Homepage from './components/homepage/Homepage'
 import Footer from './components/footer/Footer';
 import NavigationBar from './components/navigationBar/NavigationBar'
+import { UserProvider } from './context/UserContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AboutUs from './components/aboutUs/AboutUs';
+import Login from './components/auth/login/Login';
+import Register from './components/auth/register/Register';
 
 function App() {
   return (
@@ -13,7 +19,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Homepage />} />
               {/* <Route path="/" element={< />} /> */}
-              <Route path="/login" element={<LogIn />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/aboutus" element={<AboutUs />} />
             </Routes>
